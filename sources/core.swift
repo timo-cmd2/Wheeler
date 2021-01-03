6859e20256ddb6c0410d6de42528ffb224e6f28a
Timo S <58859419+timo-cmd@users.noreply.github.com>
2021-01-03 15:50:01 +0100

    added licensing banner

--- sources/core.swift
+++ sources/core.swift
@@ -1,2 +1,8 @@
+/**
+* This file is part of the Wheeler programming-language which
+* was started on 03.01.2021 by Timo Sarkar and is beeing mantained
+* since, frequently. The Wheeler programming language is licensed 
+* under the CC0 1.0 Universal License.
+*/
 import Foundation
 import Darwin
