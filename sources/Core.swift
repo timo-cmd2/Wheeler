7bb66262d363331dd2a31fb36df32d2cefca5cb1
Timo S <58859419+timo-cmd@users.noreply.github.com>
2021-01-04 12:17:03 +0100

    Remove weird code xD

--- sources/Core.swift
+++ sources/Core.swift
@@ -5,15 +5,6 @@
 * under the CC0 1.0 Universal License.
 */
 import Foundation
-import Darwin
-
-/**
-* This file is part of the Wheeler programming-language which
-* was started on 03.01.2021 by Timo Sarkar and is beeing mantained
-* since, frequently. The Wheeler programming language is licensed 
-* under the CC0 1.0 Universal License.
-*/
-import Foundation
 
 public final class WheelerMain {
     public final class Logger {
